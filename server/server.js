936aba851467a84c9d58f143a62481b6571eea21
Janine Lapiz <jglapiz@up.edu.ph>
2020-09-18 04:11:33 +0800

    Applied input validators in server.js; added Enum and default value for 'status' in schema.graphql

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express'); 
+const { ApolloServer, UserInputError } = require('apollo-server-express'); 
 const fs = require('fs');
 const { GraphQLScalarType } = require('graphql');
 const { Kind, parseValue } = require('graphql/language');
@@ -11,10 +11,14 @@ const GraphQLDate = new GraphQLScalarType({
         return value.toISOString();
     },
     parseValue(value) {     //applied { Kind, parseValue }
-        return new Date(value); 
+        const dateValue = new Date(value);
+        return isNaN(dateValue) ? undefined : dateValue; 
     },
     parseLiteral(ast) {    //applied { Kind, parseValue }
-        return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined;
+        if(ast.kind == Kind.STRING) {
+            const value = new Date(ast.value);
+            return isNaN(value) ? undefined : value;
+        }
     }
 });
 
@@ -53,17 +57,34 @@ function setAboutMessage(_, { message }) {
     return aboutMessage = message;
 }
 
+function validateIssue (issue) {
+    const errors = [];
+    if(issue.title.length < 3) {
+        errors.push('Field "title" must be at least 3 characters long.');
+    }
+    if(issue.status === 'Assigned' && !issue.owner) {
+        errors.push('Field "owner" is required when status is "Assigned".');
+    }
+    if(errors.length > 0) {
+        throw new UserInputError('Invalid input(s)', { errors });
+    }
+}
+
 function issueAdd(_, { issue }) {
+    validateIssue(issue);
     issue.id = issuesDB.length + 1;
     issue.created = new Date();
-    if(!issue.status) issue.status = 'New';
     issuesDB.push(issue);
     return issue;
 }
 
 const server = new ApolloServer({
     typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
-    resolvers
+    resolvers,
+    formatError: error => {
+        console.log(error);
+        return error;
+    }
 });
 
 const app = express();
